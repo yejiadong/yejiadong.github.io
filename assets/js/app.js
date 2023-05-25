ca467a34a7e09394cc8ff462a2cea6e84fe016b2
yejiadong <33195244+yejiadong@users.noreply.github.com>
2023-05-26 01:16:32 +0800

    Initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+function replace( show ) {
+    document.getElementById("education").style.display="none";
+    document.getElementById("international").style.display="none";
+    document.getElementById("coursework").style.display="none";
+    document.getElementById("teaching").style.display="none";
+    document.getElementById(show).style.display="block";
+}
